f8b4342413724c21686da3e37431a3ed39ec282b
Viki Harrod <victoria.harrod@gmail.com>
2015-03-30 13:25:50 -0600

    add filter for breathing problems and clean method to downcase and remove non-letters

--- triage_keyword_filter.rb
+++ triage_keyword_filter.rb
@@ -1,22 +1,41 @@
 class TriageKeywordFilter
   def emergent?(patient_symptoms)
-    @words = patient_symptoms.split(' ')
+    @words = clean(patient_symptoms.split(' '))
 
-    possible_angina?
+    possible_angina? ||
+    possible_breathing_problem?
   end
 
   private
 
+  def clean(words)
+    words.map do |word|
+      word.downcase.gsub(/[^a-z]/, '')
+    end
+  end
+
   def possible_angina?
     chest_or_left_arm? && corresponding_symptoms?(angina_symptoms)
+  end
+
+  def possible_breathing_problem?
+    breath_related? && corresponding_symptoms?(breathing_symptoms)
   end
 
   def chest_or_left_arm?
     @words.any? { |word| word == 'chest' || 'left arm' }
   end
 
+  def breath_related?
+    @words.any? { |word| word.start_with?('breath') || word == 'sob' }
+  end
+
   def angina_symptoms
     ['pain', 'pressure', 'pressing', 'squeezed', 'squeezing']
+  end
+
+  def breathing_symptoms
+    ['difficult', 'difficulty', 'short', 'shortness', 'sob']
   end
 
   def corresponding_symptoms?(symptoms)
--- triage_keyword_filter_spec.rb
+++ triage_keyword_filter_spec.rb
@@ -2,15 +2,27 @@ require 'rspec'
 require_relative 'triage_keyword_filter'
 
 describe 'triage keyword filter' do
-  it 'returns true for chest pain' do
+  it 'returns true for chest pain or pressure' do
     filter = TriageKeywordFilter.new
 
     expect(filter.emergent?('I have pain in my chest')).to eq true
+    expect(filter.emergent?('I sprained my ankle and have significant ankle pain and a rash on my chest')).to eq true
+    expect(filter.emergent?('I have a squeezing feeling in my chest')).to eq true
   end
 
-  it 'returns true for left arm pain' do
+  it 'returns true for left arm pain or pressure' do
     filter = TriageKeywordFilter.new
 
-    expect(filter.emergent?('I have pain in my left arm')).to eq true
+    expect(filter.emergent?('I have shooting pain in my left arm')).to eq true
+    expect(filter.emergent?('I have a pressing feeling in my left arm')).to eq true
+  end
+
+  it 'returns true for shortness of breath' do
+    filter = TriageKeywordFilter.new
+
+    expect(filter.emergent?('I am experiencing shortness of breath')).to eq true
+    expect(filter.emergent?('I am experiencing SOB')).to eq true
+    expect(filter.emergent?('I am experiencing s.o.b.')).to eq true
+    expect(filter.emergent?('I am having difficulty breathing')).to eq true
   end
 end
