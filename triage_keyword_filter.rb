60340ab18251f6bd211787628a38bca02b55ba30
Viki Harrod <victoria.harrod@gmail.com>
2015-03-30 10:40:44 -0600

    refactor by extracting methods

--- triage_keyword_filter.rb
+++ triage_keyword_filter.rb
@@ -1,19 +1,27 @@
 class TriageKeywordFilter
   def emergent?(patient_symptoms)
-    words = patient_symptoms.split(' ')
+    @words = patient_symptoms.split(' ')
 
-    if words.any? { |word| word == 'chest' || 'left arm' }
-      words.any? do |word|
-        angina_symptoms.any? { |symptom| symptom == word }
-      end
-    else
-      false
-    end
+    possible_angina?
   end
 
   private
 
+  def possible_angina?
+    chest_or_left_arm? && corresponding_symptoms?(angina_symptoms)
+  end
+
+  def chest_or_left_arm?
+    @words.any? { |word| word == 'chest' || 'left arm' }
+  end
+
   def angina_symptoms
     ['pain', 'pressure', 'pressing', 'squeezed', 'squeezing']
+  end
+
+  def corresponding_symptoms?(symptoms)
+    @words.any? do |word|
+      symptoms.any? { |symptom| symptom == word }
+    end
   end
 end
