4c6e0a8199b77bd8e0112f0226fb51aaa3d96d9c
Viki Harrod <victoria.harrod@gmail.com>
2015-03-30 10:31:30 -0600

    add angina_symptoms, add spec for left arm pain

--- triage_keyword_filter.rb
+++ triage_keyword_filter.rb
@@ -2,10 +2,18 @@ class TriageKeywordFilter
   def emergent?(patient_symptoms)
     words = patient_symptoms.split(' ')
 
-    if words.any? { |word| word == 'chest' }
-      words.any? { |word| word == 'pain' }
+    if words.any? { |word| word == 'chest' || 'left arm' }
+      words.any? do |word|
+        angina_symptoms.any? { |symptom| symptom == word }
+      end
     else
       false
     end
+  end
+
+  private
+
+  def angina_symptoms
+    ['pain', 'pressure', 'pressing', 'squeezed', 'squeezing']
   end
 end
--- triage_keyword_filter_spec.rb
+++ triage_keyword_filter_spec.rb
@@ -2,9 +2,15 @@ require 'rspec'
 require_relative 'triage_keyword_filter'
 
 describe 'triage keyword filter' do
-  it 'identifies chest pain' do
+  it 'returns true for chest pain' do
     filter = TriageKeywordFilter.new
 
     expect(filter.emergent?('I have pain in my chest')).to eq true
+  end
+
+  it 'returns true for left arm pain' do
+    filter = TriageKeywordFilter.new
+
+    expect(filter.emergent?('I have pain in my left arm')).to eq true
   end
 end
